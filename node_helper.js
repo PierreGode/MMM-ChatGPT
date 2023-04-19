3a0af82ca8edff6905ca71fc7e1f97d02fac8ccf
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-19 23:42:53 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,44 +1,40 @@
 const NodeHelper = require("node_helper");
-const SpeechRecognition = require("speech-recognition");
+const openai = require("openai");
+const fs = require("fs");
+const gtts = require("gtts");
 
 module.exports = NodeHelper.create({
+
   start: function () {
-    console.log("Starting node helper: " + this.name);
-    this.listening = false;
-    this.speechRecog = new SpeechRecognition();
-    this.speechRecog.setTriggerWords(["magic mirror"]);
-    const self = this;
-    this.handler = function (transcript) {
-      if (transcript.toLowerCase().includes("magic mirror")) {
-        self.listening = true;
-        self.sendSocketNotification("LISTENING", true);
-        self.speechRecog.startListening();
-      }
-    };
-    this.speechRecog.addEventListener("result", function (event) {
-      const transcript = event.results[0][0].transcript;
-      self.sendSocketNotification("TRANSCRIPT", transcript);
-    });
-    this.speechRecog.addEventListener("end", function (event) {
-      if (self.listening) {
-        self.speechRecog.startListening();
-      }
-    });
-    this.speechRecog.addEventListener("error", function (event) {
-      console.log("Speech recognition error: ", event.error);
-    });
+    console.log("Starting module: " + this.name);
+    this.config = {};
   },
-  // Override socketNotificationReceived method.
+
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "LISTENING") {
-      this.listening = payload;
-      if (this.listening) {
-        this.speechRecog.addEventListener("result", this.handler);
-        this.speechRecog.startListening();
-      } else {
-        this.speechRecog.removeEventListener("result", this.handler);
-        this.speechRecog.stopListening();
-      }
+    if (notification === "CONFIG") {
+      this.config = payload;
+      openai.apiKey = this.config.apiKey;
+    } else if (notification === "QUESTION") {
+      openai.completions.create({
+        engine: "davinci",
+        prompt: payload,
+        maxTokens: 150,
+        n: 1,
+        stop: "\n",
+      }).then((response) => {
+        let answer = response.data.choices[0].text.trim();
+        let tts = new gtts(answer);
+        tts.save("audio.mp3", () => {
+          let audio = fs.readFileSync("audio.mp3").toString("base64");
+          this.sendSocketNotification("RESPONSE", {
+            question: payload,
+            answer: answer,
+            audio: audio
+          });
+        });
+      }).catch((error) => {
+        console.error(error);
+      });
     }
   }
 });
