02d7f6cb3cfab0436bc66a0fef5b582a90691595
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-19 17:42:04 +0200

    Update MMM-ChatGTP.js

--- MMM-ChatGTP.js
+++ MMM-ChatGTP.js
@@ -1,63 +1,47 @@
 Module.register("MMM-ChatGTP", {
-  
   defaults: {
     apiKey: "",
-    triggerWord: "elsa",
+    triggerWord: ""
   },
 
-  start: function() {
-    console.log("Starting module: " + this.name);
-    this.answer = "";
-    this.question = "";
-    this.recognizing = false;
-    this.triggered = false;
-    this.audio = null;
-    this.listening = false;
-    this.connected = false;
-    this.checkDependencies();
-  },
-  checkDependencies: function() {
-    //Check for necessary dependencies
-    const dependencies = ["@google-cloud/speech", "openai", "gtts", "speaker", "lame"];
-    let missingDependencies = [];
-    for(let i = 0; i < dependencies.length; i++) {
-      try {
-        require.resolve(dependencies[i]);
-      }
-      catch(e) {
-        missingDependencies.push(dependencies[i]);
-      }
-    }
-    if(missingDependencies.length > 0) {
-      console.error(`[${this.name}] ERROR: Missing dependencies: ${missingDependencies.join(", ")}.`);
-      return;
-    }
-    this.connected = true;
-    this.recognize();
-  },
-  recognize: function() {
-    //Set up speech recognition
-    const speech = require('@google-cloud/speech');
-    const client = new speech.SpeechClient();
-    const request = {
-      config: {
-        encoding: 'LINEAR16',
-        sampleRateHertz: 16000,
-        languageCode: 'en-US',
-      },
-      interimResults: false,
+  start: function () {
+    Log.info("Starting module: " + this.name);
+    this.sendSocketNotification("CONFIG", this.config);
+    this.recognition = new webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.recognition.onstart = () => {
+      console.log("Speech recognition started");
     };
-    const recognizeStream = client.streamingRecognize(request)
-    .on('error', console.error)
-    .on('data', data => {
-      if(data.results[0] && data.results[0].alternatives[0]) {
-        const transcript = data.results[0].alternatives[0].transcript.toLowerCase();
-        if(transcript.includes(this.config.triggerWord) && !this.triggered) {
-          this.triggered = true;
-          this.sendSocketNotification("TRIGGERED", {});
-          this.listening = true;
-          this.startRecognition();
-        }
+    this.recognition.onresult = (event) => {
+      let last = event.results.length - 1;
+      let transcript = event.results[last][0].transcript.toLowerCase().trim();
+      if (transcript.startsWith(this.config.triggerWord)) {
+        this.sendSocketNotification("QUESTION", transcript.slice(this.config.triggerWord.length).trim());
       }
-    });
+    };
+    this.recognition.onend = () => {
+      console.log("Speech recognition ended");
+      this.recognition.start();
+    };
+    this.recognition.start();
   },
+
+  getDom: function () {
+    this.wrapper = document.createElement("div");
+    this.wrapper.className = "small";
+    this.content = document.createElement("div");
+    this.content.className = "bright";
+    this.wrapper.appendChild(this.content);
+    return this.wrapper;
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "RESPONSE") {
+      this.content.innerHTML = "<p>You asked: " + payload.question + "</p>" +
+        "<p>The answer is: " + payload.answer + "</p>";
+      let audio = new Audio("data:audio/mp3;base64," + payload.audio);
+      audio.play();
+    }
+  }
+});
