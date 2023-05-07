d75ca849b62abf2c1b43a307a1fd88b6999ce3d6
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-05-07 22:15:26 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,40 +1,38 @@
 const NodeHelper = require("node_helper");
-const openai = require("openai");
-const fs = require("fs");
-const gtts = require("gtts");
+const { spawn } = require("child_process");
 
 module.exports = NodeHelper.create({
-
   start: function () {
-    console.log("Starting module: " + this.name);
-    this.config = {};
+    this.pythonProcess = null;
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "CONFIG") {
-      this.config = payload;
-      openai.apiKey = this.config.apiKey;
-    } else if (notification === "QUESTION") {
-      openai.completions.create({
-        engine: "davinci",
-        prompt: payload,
-        maxTokens: 150,
-        n: 1,
-        stop: "\n",
-      }).then((response) => {
-        let answer = response.data.choices[0].text.trim();
-        let tts = new gtts(answer);
-        tts.save("audio.mp3", () => {
-          let audio = fs.readFileSync("audio.mp3").toString("base64");
-          this.sendSocketNotification("RESPONSE", {
-            question: payload,
-            answer: answer,
-            audio: audio
-          });
-        });
-      }).catch((error) => {
-        console.error(error);
-      });
+    if (notification === "START_CHATGPT") {
+      this.startPythonScript();
     }
-  }
+  },
+
+  startPythonScript: function () {
+    const self = this;
+    const pythonScript = "../MMM-ChatGPT/Chat.py";
+    this.pythonProcess = spawn("python3", [pythonScript]);
+
+    this.pythonProcess.stdout.on("data", (data) => {
+      self.sendSocketNotification("CHATGPT_RESPONSE", data.toString());
+    });
+
+    this.pythonProcess.stderr.on("data", (data) => {
+      console.error(`Python script error: ${data}`);
+    });
+
+    this.pythonProcess.on("close", (code) => {
+      console.log(`Python script exited with code ${code}`);
+    });
+  },
+
+  stop: function () {
+    if (this.pythonProcess) {
+      this.pythonProcess.kill();
+    }
+  },
 });
